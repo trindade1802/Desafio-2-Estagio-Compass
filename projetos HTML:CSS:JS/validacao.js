b37ca83a54e0d0756953a512b3bbfbb576c14eef
trindade1802 <109542400+trindade1802@users.noreply.github.com>
2022-07-18 13:33:46 -0300

    Add files via upload

---
+++ projetos HTML:CSS:JS/validacao.js
@@ -0,0 +1,13 @@
+var form1 = document.querySelector("parede1")
+
+var largura = form1.largura.value
+var altura = form1.altura.value
+var portas = form1.portas.value
+var janelas = form1.janelas.value
+
+function validaArea(input){
+
+  var Area = largura * altura
+
+  const areaRecebida =
+}
